73adf1b2f25b03843dadf7b2bdcd800f7e5f4ead
Matthieu Moquet <matthieu@moquet.net>
2013-11-14 11:35:39 +0100

    Create a gh-pages deploy script

---
+++ gh-pages.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Update gh-pages branch
+git branch -D gh-pages
+git checkout -b gh-pages master
+
+# Compile assets
+grunt build
+git add -f build
+git commit -m "Build GH pages"
+
+# Push & reset
+git push -f origin gh-pages
+git checkout master
